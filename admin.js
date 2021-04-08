4b1c9671f7f7728612d4dbb2b157c18e8c044aa7
Mani Movassagh <mani.mgh@gmail.com>
2021-04-08 16:01:43 +0200

    added add form!

--- admin.js
+++ admin.js
@@ -14,7 +14,6 @@ function ComeOnScreen(elementInnerHtml) {
 }
 //function to prevent async undefined!!
 const dataReturnAdmin = (data, id) => {
-  console.log(data);
   let admintable = `
   <tr>
     <th scope="row">${data.title}</th>
@@ -28,3 +27,40 @@ const dataReturnAdmin = (data, id) => {
 `;
   ComeOnScreen(admintable);
 };
+
+const toggler = document.querySelector("#inputshop");
+const addtoShop = document.querySelector("#btnAdd");
+// console.log(toggler);
+// console.log(addtoShop);
+addtoShop.addEventListener("click", () => {
+  if (toggler.className == "d-none") {
+    toggler.className = "d-inline";
+  } else {
+    toggler.className = "d-none";
+  }
+});
+
+
+
+//grab form data from adding form
+const btn=document.querySelector('#submit')
+console.log(btn);
+btn.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log(e.target);
+})
+
+
+//check manualy add item to firebase first
+// db.collection("items")
+//   .doc().set({
+//     brand: "some watch",
+//     price: 440,
+//     title: "model new lorem ippsum",
+//   })
+//   .then(() => {
+//     console.log("Document successfully written!");
+//   })
+//   .catch((error) => {
+//     console.error("Error writing document: ", error);
+//   });
